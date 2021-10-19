cf6c7ccbd62bdd0f5eb361df90af6c3b47c41a1c
julio11lp <julio11lp@hotmail.com>
2021-10-19 17:43:20 -0300

    Updates

---
+++ precache-manifest.2be544b0549a0ef9a087c367c8439493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e759df9e41f16dcd57d6335a5fbeec",
+    "url": "/StarWars-Table/index.html"
+  },
+  {
+    "revision": "3819c3c8a4ad831b4ee2",
+    "url": "/StarWars-Table/static/css/main.0ff2c4cd.chunk.css"
+  },
+  {
+    "revision": "8db6f97ccf855b2404b6",
+    "url": "/StarWars-Table/static/js/2.3892ea97.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/StarWars-Table/static/js/2.3892ea97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3819c3c8a4ad831b4ee2",
+    "url": "/StarWars-Table/static/js/main.69d0ede9.chunk.js"
+  },
+  {
+    "revision": "f08c411b2057b58738a7",
+    "url": "/StarWars-Table/static/js/runtime-main.7e3ecbac.js"
+  }
+]);
